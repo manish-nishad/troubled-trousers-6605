962ec59e16caeb5802f9db4258c7ee45465dc997
Manish <maknish1993@gmail.com>
2022-12-10 22:39:45 +0530

    Initial commit

--- nordstrom/src/Routes/AllRoutes.jsx
+++ nordstrom/src/Routes/AllRoutes.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import React from 'react';
+import { Routes, Route } from "react-router-dom";
+import HomePage from '../Pages/HomePage';
+import ProductPage from '../Pages/ProductPage';
 
 const AllRoutes = () => {
   return (
-    <div>AllRoutes</div>
+    <div>
+      <Routes>
+        <Route path='/' element={<HomePage />}></Route>
+        <Route path='/productPage' element={<ProductPage />}></Route>
+      </Routes>
+    </div>
   )
 }
 
