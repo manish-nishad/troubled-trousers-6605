0561ab5c69fae83aeb50ea5d43b8e56add737609
Manish <maknish1993@gmail.com>
2022-12-13 19:07:53 +0530

    Initial commit

--- nordstrom/src/App.js
+++ nordstrom/src/App.js
@@ -1,5 +1,4 @@
-import logo from './logo.svg';
-import './App.css';
+ import './App.css';
 import AllRoutes from './Routes/AllRoutes';
 import { BrowserRouter } from 'react-router-dom';
 import Navbar from "./Components/Navbar";
